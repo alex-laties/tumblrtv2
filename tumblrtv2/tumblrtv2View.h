28e81a389aed3ff7311f5553658a4affe8091582
Alex Laties <agl@tumblr.com>
2019-03-14 15:09:40 -0400

    Initial Commit

---
+++ tumblrtv2/tumblrtv2View.h
@@ -0,0 +1,13 @@
+//
+//  tumblrtv2View.h
+//  tumblrtv2
+//
+//  Created by Alexander Laties on 3/14/19.
+//  Copyright © 2019 webscaleaf. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface tumblrtv2View : ScreenSaverView
+
+@end
